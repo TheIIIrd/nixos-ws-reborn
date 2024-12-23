a54914fc0d308ada1d78677df5972ee18d4fb703
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-12-23 11:32:06 +0300

    Added nixpkgs-unstable to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   description = "My system configuration";
 
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    stylix.url = "github:danth/stylix";
 
     # COMING SOON...
     #nixvim = {
@@ -19,7 +18,7 @@
     #};
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: let
     system = "x86_64-linux";
     homeStateVersion = "24.11";
     user = "amper";
@@ -57,5 +56,7 @@
         ./home-manager/home.nix
       ];
     };
+
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
   };
 }
